cd594eaf8a4a0422cb28a1eb0349f3c88a2feaa2
Miranda <m.jack@unb.ca>
2014-07-14 14:25:23 -0300

    Practicing how to make lists

---
+++ lists.py
@@ -0,0 +1,16 @@
+# print range (10)
+# for x in range(3):
+#     print 'x=',x
+    
+names = [['dave',23,True],['jeff',24,False],['noel',21,True]]
+for x in names:
+    None
+   
+y = list() 
+for x in range(3):
+    y.append(x**2)
+print y
+
+y = [i+3 for i in y]
+print y
+    
