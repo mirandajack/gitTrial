780081b2bc0f0125a7173fef706eb92316dfa20f
Miranda <m.jack@unb.ca>
2014-07-14 14:34:30 -0300

    If... then w/ Jeff and Dave

---
+++ ifTrial.py
@@ -0,0 +1,19 @@
+daveAge = 23
+jeffAge = 23
+
+# if daveAge>jeffAge:
+#     print 'Dave is older than Jeff'
+# if jeffAge>daveAge:
+#     print 'Jeff is older than Dave'
+         
+# if daveAge>jeffAge:
+#     print 'Dave is older than Jeff'
+# else:
+#     print 'Jeff is older than Dave'
+    
+if daveAge>jeffAge:
+    print 'Dave is older than Jeff'
+elif jeffAge>daveAge:
+    print 'Jeff is older than Dave'
+else:
+    print 'Jeff and Dave are the same age'
